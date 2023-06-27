1edbfeec45b3b4bfa0d79c7cd0ee0e5c50504829
Young-Gaius <youngehu8@gmail.com>
2023-06-27 13:33:08 +0100

    fix linter error

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const hamburger = document.querySelector('.hamburgers')
-const popupMenu = document.querySelector('.popup-menu')
-const closePopup = document.querySelector('.close')
-const popupLinks = document.querySelectorAll('.popup-link')
+const hamburger = document.querySelector('.hamburgers');
+const popupMenu = document.querySelector('.popup-menu');
+const closePopup = document.querySelector('.close');
+const popupLinks = document.querySelectorAll('.popup-link');
 
 hamburger.addEventListener('click', () => {
-  popupMenu.classList.toggle('hide-popup')
-})
+  popupMenu.classList.toggle('hide-popup');
+});
 
 closePopup.addEventListener('click', () => {
-  popupMenu.classList.toggle('hide-popup')
-})
+  popupMenu.classList.toggle('hide-popup');
+});
 
-popupLinks.forEach( popupLink => {
+popupLinks.forEach((popupLink) => {
   popupLink.addEventListener('click', () => {
-    popupMenu.classList.add('hide-popup')
-  })
-})
+    popupMenu.classList.add('hide-popup');
+  });
+});
