be06ada34a1938ad3abc8cae051a0062639cb417
Young-Gaius <youngehu8@gmail.com>
2023-06-28 23:04:52 +0100

    add html

--- index.js
+++ index.js
@@ -16,3 +16,80 @@ popupLinks.forEach((popupLink) => {
     popupMenu.classList.add('hide-popup');
   });
 });
+
+// caed section
+
+const card = [{
+  title1: "Multi-Post Stories",
+  image: "images-3/mobile.svg",
+  title2: "Gain + Glory",
+  technology: ["Ruby on Rails","CSS","Javascript","HTML"],
+  button: "button"
+},
+{
+  title1: "Multi-Post Stories",
+  image: "images-3/mobile.svg",
+  title2: "Gain + Glory",
+  technology: ["Ruby on Rails","CSS","Javascript","HTML"],
+  button: "button"
+},
+{
+  title1: "Multi-Post Stories",
+  image: "images-3/mobile.svg",
+  title2: "Gain + Glory",
+  technology: ["Ruby on Rails","CSS","Javascript","HTML"],
+  button: "button"
+},
+{
+  title1: "Multi-Post Stories",
+  image: "images-3/mobile.svg",
+  title2: "Gain + Glory",
+  technology: ["Ruby on Rails","CSS","Javascript","HTML"],
+  button: "button"
+},
+{
+  title1: "Multi-Post Stories",
+  image: "images-3/mobile.svg",
+  title2: "Gain + Glory",
+  technology: ["Ruby on Rails","CSS","Javascript","HTML"],
+  button: "button"
+},
+{
+  title1: "Multi-Post Stories",
+  image: "images-3/mobile.svg",
+  title2: "Gain + Glory",
+  technology: ["Ruby on Rails","CSS","Javascript","HTML"],
+  button: "button"
+}
+
+]
+const cardSection = document.querySelector('.all-card-holders');
+
+function generateCardSection(){
+  card.forEach(card => {
+    cardSection.innerHTML += 
+    `
+    <article class="card-holder">
+            <div class="card">
+                <div class="empty-box">
+                 <img src="${card.image}">
+                </div>
+                <div class="multi-post-stories">
+                    <h3>${card.title1} <br> ${card.title2}</h3>
+                    <div class="programing-languages">
+                        <ul>
+                            <li>${card.technology[0]}</li>
+                            <li>${card.technology[1]}</li>
+                            <li>${card.technology[2]}</li>
+                            <li>${card.technology[3]}</li>
+                        </ul>
+                    </div>
+                    <button class="green-button interaction-btn">${card.button}</button>
+                </div>
+        </article>
+        <div><img src="${card.image}"</div>
+    `
+  }
+)}
+
+generateCardSection()
